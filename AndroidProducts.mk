7e99d4b26abbc5455c05fcb55c53d1dd63f20510
Dan Willemsen <dwillemsen@google.com>
2018-04-04 16:49:07 -0700

    mocha: Switch from add_lunch_combo to COMMON_LUNCH_CHOICES
    Bug: 77599627
    Test: lunch  [see entries in list]
    Change-Id: Id44fcfec47eeb979a745f879031f1bec7359a633

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2017 The Android Open-Source Project
+# Copyright (C) 2018-2020 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,3 +16,7 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_mocha.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_mocha-userdebug \
+    lineage_mocha-eng
