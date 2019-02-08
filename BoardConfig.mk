9222452718b0b616e33afd20a5590025fd74a7b2
Anton Hansson <hansson@google.com>
2019-02-08 17:56:34 +0000

    mocha: Use generic arch variant
    Follow blueline which has not demonstrated any regressions
    after switching to a generic arch variant.
    
    Bug: 120773446
    Test: make
    Change-Id: I9e23963fcfa5ccaa912f2930a632458c4d2a692d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -24,7 +24,8 @@ TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv7-a-neon
-TARGET_CPU_VARIANT := cortex-a15
+TARGET_CPU_VARIANT := generic
+TARGET_TEGRA_VERSION := t124
 
 # Audio
 BOARD_USES_GENERIC_AUDIO := false
